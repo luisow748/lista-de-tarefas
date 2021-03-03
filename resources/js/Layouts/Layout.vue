f1572a242f97850e38217d6a6badeb7b9d0de3b0
luisow748 <luisow748@gmail.com>
2021-03-03 08:22:54 -0300

    instalacao laravel vue e inertia js

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <header>
+      <inertia-link href="/">Home</inertia-link>
+      <inertia-link href="/sobre">About</inertia-link>
+      <inertia-link href="/contato">Contact</inertia-link>
+    </header>
+    <article>
+      <slot />
+    </article>
+  </main>
+</template>
+
+<script>
+  export default {
+     
+    props: {
+      title: String,
+    },
+    watch: {
+      title: {
+        immediate: true,
+        handler(title) {
+          document.title = title
+        },
+      },
+    },
+  }
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+<layout>
+	<div id="app">
+		<h4>Lista de Tarefas - Pulse</h4>
+		
+	</div>
+</layout>
+</template>
+
+<script>
+import Layout from '../Layouts/Layout'
+export default {
+	 components:{
+          Layout
+      },
+	
+}
+</script>
+
+<style>
+	#app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
