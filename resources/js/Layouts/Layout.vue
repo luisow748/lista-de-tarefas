87c87735f2415bdc0f49dc8f0f66cde8615d3ac5
luisow748 <luisow748@gmail.com>
2021-03-03 09:30:36 -0300

    adicionada Navbar no Layout.vue

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,21 +1,26 @@
 <template>
-
+<div>
   <main>
+  <Navbar />
     <header>
       <inertia-link href="/">Home</inertia-link>
       <inertia-link href="/sobre">About</inertia-link>
       <inertia-link href="/contato">Contact</inertia-link>
     </header>
-    
+
     <article>
       <slot />
     </article>
   </main>
+</div>
 </template>
 
 <script>
+import Navbar from '../Components/Estruturas/Navbar'
   export default {
-     
+      components: {
+          Navbar
+      },
     props: {
       title: String,
     },
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,18 +2,19 @@
 <layout>
 	<div id="app">
 		<h4>Lista de Tarefas - Pulse</h4>
-		
+
 	</div>
 </layout>
 </template>
 
 <script>
 import Layout from '../Layouts/Layout'
+
 export default {
 	 components:{
           Layout
       },
-	
+
 }
 </script>
 
