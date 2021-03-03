bda9663395328d54d8defbff5ef1d798cee102c1
luisow748 <luisow748@gmail.com>
2021-03-03 09:22:24 -0300

    insercao relacionamentos tarefas e tipos de tarefas

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\TipoDeTarefa;
 
 class Tarefa extends Model
 {
     use HasFactory;
+    public function tipodetarefas()
+    {
+        return $this->belongsTo(TipoDeTarefa::class);
+    }
 }
--- app/Models/TipoDeTarefa.php
+++ app/Models/TipoDeTarefa.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Tarefa;
 
 class TipoDeTarefa extends Model
 {
     use HasFactory;
+    public function tarefas()
+    {
+        return $this->hasMany(Tarefa::class);
+    }
 }
