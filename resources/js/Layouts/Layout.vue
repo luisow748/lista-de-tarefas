8d9d01e909955103b07c6660ff4ebf38707b6f7e
Luís Porto <luisow748@gmail.com>
2021-03-06 19:24:32 -0300

    correção tag <title>

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -14,9 +14,10 @@
 <script>
 import Navbar from '../Pages/Components/Estruturas/Navbar'
   export default {
-      components: {
+    title: 'Tarefas',
+    components: {
           Navbar
-      },
+    },
     props: {
       title: String,
     },
